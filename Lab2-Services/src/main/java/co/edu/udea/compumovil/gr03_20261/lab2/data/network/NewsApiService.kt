5b669e5adebbb101df104c9782120c57e1b2f32e
SPelaez122 <sebastianpz122@gmail.com>
2026-06-05 22:23:06 -0500

    Country of the news changed, API is now functional

--- Lab2-Services/src/main/java/co/edu/udea/compumovil/gr03_20261/lab2/data/network/NewsApiService.kt
+++ Lab2-Services/src/main/java/co/edu/udea/compumovil/gr03_20261/lab2/data/network/NewsApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 interface NewsApiService {
     @GET("v2/top-headlines")
     suspend fun getHeadlines(
-        @Query("country") country: String = "co",
+        @Query("country") country: String = "us",
         @Query("pageSize") pageSize: Int = 20,
         @Query("apiKey") apiKey: String = "873618d0bc114d5d8c0f88e08a46e154"
     ): NewsResponse
